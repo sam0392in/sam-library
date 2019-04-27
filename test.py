959ccb96f4c6b4a4e2da8b2f17e3efbc1a92153d
Samarth Kanungo <samarthkanungo0392in@gmail.com>
2019-04-27 20:46:33 +0530

    sam Library v1.0

---
+++ test.py
@@ -0,0 +1,55 @@
+from flask import Flask, redirect, url_for, render_template, request
+import pymongo
+
+server = pymongo.MongoClient("mongodb://mongo:27017/")
+mydb = server["samlibrary"]
+mycol = mydb["books"]
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template(login.html)
+
+@app.route('/login', methods = ['GET' , 'POST'])
+   if request.method == 'POST':
+     
+	 user_name = request.form['loginname']
+	 
+	 if user_name == 'admin':
+	   return render_template('admin.html')
+	 
+     elif user_name == '':
+       return redirect(url_for('index')
+
+     else:
+	   return render_template('homepage.html',username = user_name)
+
+
+@app.route('/home')
+def home():
+  if request.method == 'POST':
+    
+    bookname = request.form['bookname']
+    author = request.form['author']
+    	
+	if bookname =! '' && author == '':
+	  return redirect(url_for('read', bookname=bookname))
+	  
+	elif bookname == '' && author != '':
+      return redirect(url_for('listbooks', author=author))
+
+	else:
+       return redirect(url_for('exact',bookname=bookname, author=author ))
+
+	   
+@app.route('/read/<bookname>')
+def read(bookname):
+  query = {"title": "%s" %bookname}
+  content = mycol.find(query)
+  
+  for text in content:
+    x = (text["story"])
+	
+  return render_template('read.html', content = x)	   
+ 
